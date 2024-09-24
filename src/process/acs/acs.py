5eecf2567b2ae95cdae8b95aacef9d7c9fc8abea
Lim Jian Ye <jianye_lim@outlook.com>
2024-09-25 01:47:28 +0800

    Updated acs to take new format

--- src/process/acs/acs.py
+++ src/process/acs/acs.py
@@ -12,7 +12,30 @@ import pandas as pd
 from PyPDF2 import PdfReader
 
 # Custom
-from .acs_utils import add_data, extract_data, get_data, get_totals
+from .acs_utils import add_data, get_data, get_totals
+
+##################
+# Configurations #
+##################
+
+# Pattern to match for invoice details
+inv_pattern = re.compile(
+    r'\s*(?P<date>\d{2}/\d{2}/\d{4})'                       # Date pattern
+    r'\s*(?P<do_no>\w{2} \d{8})'                            # DO NO.
+    r'\s*(?P<desc>.*?)'                                     # Description
+    r'\s*(?P<qty>(?:\d{1,3},)*(?:\d+)(?:\.\d{1,2})?)\s*CU'  # Quantity
+    r'\s*(?P<unit_price>(?:\d{1,3},)*(?:\d+)(?:\.\d{2})?)'  # Unit Price
+    r'\s*(?P<inv_amt>(?:\d{1,3},)*(?:\d+)(?:\.\d{2})?)'     # Invoice Amount
+)
+
+# Pattern for location and subcon
+loc_subcon_pattern = re.compile(
+    r'\s*(?P<project>[A-Za-z]+)'                    # Project
+    r'\s*@'
+    r'\s*(?P<location>[A-Z\s\.]+)-'                 # Location
+    r'\s*(?P<subcon>[A-Z]+)'                        # Subcontractor
+    r'\s*Contract\s*No\s*:\s*(?P<contact>\d+)\s*'  # Contact number
+)
 
 #############
 # Functions #
@@ -31,35 +54,28 @@ def process_pdf(df_all, pdf_file_paths):
     """
     # Initialize headers
     data_headers = [
-        'Ref No.', 'Date', 'Description', 'Total Qty', 'Unit', 'Unit Price', 'Subtotal Amount', 
-        'Total Amt per Inv', 'For Month (YYYY MM)', 'Invoice No.', 'Zone', 'Specific Location', 
-        'Location', 'Contact No.', 'Ordered By / Remarks', 'For TAK or Subcon? [Pintary/BBR/KKL...etc]', 
-        'DO Date', 'DO No.', 'Description3', 'Code1', 'Code2', 'Code3', 'Code4', 'Qty', 'Subtotal (S$)'
-        ]
-
-    # Dictionary to store values
-    subcons = {
-        'SRM': 'SRM',
-        'FATT HENG': 'FATT HENG',
-        'CHIAN TECK': 'CT',
-        'SIONG': 'SIONG'
-    }
-
-    sites = {
-        'OB/LAB': 'OB',
-        'FAB': 'FAB'
-    }
-
-    # Pattern to match for entries
-    pattern = re.compile(
-        r'(\d{2}/\d{2}/\d{4})'                   # Date pattern
-        r' (\w{2} \d{8})'                        # Alphanumeric pattern
-        r' (.*?)'                                # Lazy matching any character
-        r' ((?:\d{1,3},)*(?:\d+)(?:\.\d{1,2})?)' # Number with optional comma and decimal
-        r' (CU)'                                 # Literal "CU"
-        r' ((?:\d{1,3},)*(?:\d+)(?:\.\d{2})?)'   # Number with optional comma and decimal
-        r' ((?:\d{1,3},)*(?:\d+)(?:\.\d{2})?)'   # Number with optional comma and decimal
-    )
+        "Inv No.",
+        "Date",
+        "Description",
+        "Total Qty",
+        "Unit",
+        "Unit Rate",
+        "Subtotal Amount",
+        "Total Amt per Inv",
+        "For Month (YYYY MM)",
+        "Zone",
+        "Size",
+        "Ordered by TAK or Subcon? [Pintary/ BBR/ KKL..etc]",
+        "DO Date",
+        "DO No.",
+        "Description2",
+        "Code1",
+        "Code2",
+        "Code3",
+        "Code4",
+        "Qty",
+        "Vendor Invoice Amount",
+    ]
 
     # Loop through all PDF files
     for f in pdf_file_paths:
@@ -67,7 +83,7 @@ def process_pdf(df_all, pdf_file_paths):
 
         # Initialize variables
         df_data = pd.DataFrame(columns=data_headers)
-        ref_no = None
+        inv_no = None
         date = None
         subcon = None
         contents = list()
@@ -80,9 +96,9 @@ def process_pdf(df_all, pdf_file_paths):
 
             for i in range(len(lines)):
                 # Get reference number
-                if ref_no is None:
+                if inv_no is None:
                     if ('INVOICE NO' in lines[i].upper()) and (len(lines[i].split(' ')[-1]) == 6):
-                        ref_no = lines[i].split(' ')[-1]
+                        inv_no = lines[i].split(' ')[-1]
 
                 # Get invoice date
                 if date is None:
@@ -91,21 +107,22 @@ def process_pdf(df_all, pdf_file_paths):
                         date = pd.to_datetime(date, format='%d/%m/%Y').strftime('%d %b %Y')
 
                 # Get subcon and location
-                if ('UMC' in lines[i].upper()) and ('@' not in lines[i].upper()) and (subcon is None):
-                    subcon = extract_data(lines[i].upper(), subcons)
-                    location = extract_data(lines[i].upper(), sites)          
+                match = re.search(loc_subcon_pattern, lines[i])
+                if match:
+                    subcon = match.group("subcon").strip().upper() if not None else ""
+                    location = match.group("location").strip() if not None else ""
 
                 # Get invoice details
-                match = pattern.match(lines[i])
+                match = inv_pattern.match(lines[i])
                 if match:
-                    do_line = list(match.groups())
-                    do_date = pd.to_datetime(do_line[0], format='%d/%m/%Y').strftime('%d %b %Y')
-                    do_mth = pd.to_datetime(do_line[0], format='%d/%m/%Y').strftime('%Y %m')
-                    do_no = do_line[1]
-                    do_desc = do_line[2]
-                    do_qty = do_line[3]
-                    do_unitprice = do_line[5]
-                    contents.append([do_mth, do_date, do_no, do_desc, do_qty, do_unitprice])
+                    do_date = pd.to_datetime(match.group("date"), format='%d/%m/%Y').strftime('%d %b %Y')
+                    do_mth = pd.to_datetime(match.group("date"), format='%d/%m/%Y').strftime('%Y %m')
+                    do_no = match.group("do_no").strip().upper().replace(" ", "") if not None else ""
+                    do_desc = match.group("desc").strip() if not None else ""
+                    do_qty = match.group("qty").strip() if not None else ""
+                    do_unitprice = match.group("unit_price").strip() if not None else ""
+                    do_invamt = float(match.group("inv_amt").strip().replace(",", ""))
+                    contents.append([do_mth, do_date, do_no, do_desc, do_qty, do_unitprice, do_invamt])
 
                 # Get underload charges
                 if 'UNDERLOAD CHARGES' in lines[i].upper():
@@ -114,7 +131,10 @@ def process_pdf(df_all, pdf_file_paths):
                         previous[0], 
                         previous[1], 
                         previous[2], 
-                        previous[3] + f' - UNDERLOAD CHARGES - {float(previous[4])}m3', '1', lines[i].split(' ')[-1]
+                        previous[3] + f' - UNDERLOAD CHARGES - {float(previous[4])}m3',
+                        '1',
+                        lines[i].split(' ')[-1],
+                        previous[-1],
                     ]
                     contents.append(underload)
 
@@ -131,15 +151,45 @@ def process_pdf(df_all, pdf_file_paths):
         df_data = df_data.reindex(range(total_rows))
 
         # Get data from contents
-        for_month, do_date, do_no, description3, qty = get_data(contents)
+        (
+            for_month,
+            do_date,
+            do_no,
+            description2,
+            qty,
+            amount,
+            code_1,
+            code_2,
+            code_3,
+            code_4,
+        ) = get_data(contents)
 
         # Add data to dataframe
         df_data = add_data(
-            df_data, unique_rows, pricings, total_qty, for_month, do_date, 
-            do_no, description3, qty, ref_no, date, sub_total, location, subcon
+            df_data=df_data,
+            unique_rows=unique_rows,
+            pricings=pricings,
+            total_qty=total_qty,
+            for_month=for_month,
+            do_date=do_date,
+            do_no=do_no,
+            description2=description2,
+            qty=qty,
+            amount=amount,
+            code_1=code_1,
+            code_2=code_2,
+            code_3=code_3,
+            code_4=code_4,
+            inv_no=inv_no,
+            date=date,
+            sub_total=sub_total,
+            subcon=subcon,
         )
+
+        # Add empty row
         df_data.loc[total_rows] = pd.Series(dtype='object')
 
+        # Append data to df_all
         if df_all is None:
             df_all = df_data
         else:
@@ -176,17 +226,20 @@ def process_excel(excel_file_path):
     else:
         raise ValueError("Header row not found in Excel file!")
 
-    # Change header to match extracted results from PDFs
-    df_xlsx = df_xlsx.rename(
-        columns={
-            "TICKET NUMBER": "DO No.",
-            "SITE CONTACT NO": "Contact No.",
-            "STRUCTURAL ELEMENT": "Specific Location",
-            "PURCHASER REPRESENTATIVE": "Ordered By / Remarks",
-        }
-    )
+    # Make a new dataframe to store the extracted data from input Excel
+    df_comments = pd.DataFrame({
+        "DO No.": df_xlsx["TICKET NUMBER"],
+        "Purchaser Personnel Contact": df_xlsx["SITE CONTACT NO"],
+        "Bored Pile No.: OR Location ***": df_xlsx["STRUCTURAL ELEMENT"],
+        "Purchaser Personnel Name": df_xlsx["PURCHASER REPRESENTATIVE"],
+    })
 
-    return df_xlsx
+    # Remove NaN rows and header
+    df_comments = df_comments.dropna(subset=['DO No.'])
+    df_comments = df_comments.iloc[1:]
+    df_comments.reset_index(drop=True, inplace=True)
+
+    return df_comments
 
 
 def acs_main(pdf_file_paths, excel_file_paths):
@@ -208,13 +261,42 @@ def acs_main(pdf_file_paths, excel_file_paths):
 
     # Process excel file, if any
     if len(excel_file_paths) > 0:
-        df_xlsx = process_excel(excel_file_paths[0])
+        df_comments = process_excel(excel_file_paths[0])
 
-        for _, row in df_xlsx.iterrows():
-            # Get results rows where DO No. matches our current row
-            mask = df_all["DO No."] == row["DO No."]
-            df_all.loc[mask, "Contact No."] = row["Contact No."]
-            df_all.loc[mask, "Specific Location"] = row["Specific Location"]
-            df_all.loc[mask, "Ordered By / Remarks"] = row["Ordered By / Remarks"]
+        # Merge df_all and df_comments based on DO No.
+        df_all = pd.merge(df_all, df_comments, how='left', on='DO No.')
+
+    # Add empty column for future use
+    df_all["Size"] = None
+
+    # Reorder columns
+    df_all = df_all[
+        [
+            "Inv No.",
+            "Date",
+            "Description",
+            "Total Qty",
+            "Unit",
+            "Unit Rate",
+            "Subtotal Amount",
+            "Total Amt per Inv",
+            "For Month (YYYY MM)",
+            "Zone",
+            "Purchaser Personnel Name",
+            "Purchaser Personnel Contact",
+            "Bored Pile No.: OR Location ***",
+            "Size",
+            "Ordered by TAK or Subcon? [Pintary/ BBR/ KKL..etc]",
+            "DO Date",
+            "DO No.",
+            "Description2",
+            "Code1",
+            "Code2",
+            "Code3",
+            "Code4",
+            "Qty",
+            "Vendor Invoice Amount",
+        ]
+    ]
 
     return df_all
--- src/process/acs/acs_utils.py
+++ src/process/acs/acs_utils.py
@@ -4,8 +4,43 @@
 # Required Modules #
 ####################
 
+# Generic/Built-in
+import re
+
 # Libs
 import pandas as pd
+
+##################
+# Configurations #
+##################
+
+# Pattern to match description for code column
+description_pattern = re.compile(
+    r'(?P<description>.*?)'                # Captures everything in front
+    r'(?:.*?\b(?P<duration>\d+\s*[Hh]?[Rr]?)\b)?'  # Optionally captures retardation duration
+    r'(?:.*?\b(?P<rtd>RTD)\b)?'            # Optionally captures 'RTD'
+)
+
+# Dictionary for different concrete grades
+grade_dict = {
+    "10": "C12/10",
+    "15": "C12/15",
+    "20": "C16/20",
+    "25": "C20/25",
+    "30": "C25/30",
+    "35": "C28/35",
+    "40": "C32/40",
+    "45": "C35/45",
+    "50": "C40/50",
+    "55": "C45/55",
+    "60": "C50/60",
+}
+
+# Dictionary of zones for each subcon
+zone_dict = {
+    "CSBP": "A",
+    "BBR": "B",
+}
 
 #############
 # Functions #
@@ -52,9 +87,30 @@ def extract_data(line, data_dict):
     return ''
 
 
+def extract_description(desc):
+    """
+    Extract data from description for code columns in resulting Excel.
+
+    Args:
+        desc (str): Description2 value
+
+    Returns:
+        rtd (str): RTD if retardant was used, else ""
+        duration (str): Duration of retardation
+    """
+    match = re.search(description_pattern, desc)
+    if match:
+        duration = match.group("duration") if not None else ""
+        rtd = match.group("rtd") if not None else ""
+    if "R" not in duration:
+        duration = duration.upper() + "R"
+    
+    return rtd, duration
+
+
 def get_data(contents):
     """
-    Get data from contents
+    Get data from contents.
 
     Args:
         contents (list): List of lists containing invoice details
@@ -63,78 +119,120 @@ def get_data(contents):
         for_month (list): List of for_month
         do_date (list): List of do_date
         do_no (list): List of do_no
-        description3 (list): List of description3
+        description2 (list): List of description2
         qty (list): List of qty
+        amount (list): List of invoice amounts
+        code_1 (list): List of concrete grades
+        code_2 (list): List of slump values
+        code_3 (list): List of retardant used
+        code_4 (list): List of retardant duration
     """
     for_month = list()
     do_date = list()
     do_no = list()
-    description3 = list()
+    description2 = list()
     qty = list()
+    amount = list()
+    code_1 = list()
+    code_2 = list()
+    code_3 = list()
+    code_4 = list()
 
     for sublist in contents:
         for_month.append(sublist[0])
         do_date.append(sublist[1])
-        do_no.append(sublist[2].replace(" ", ""))  # Remove spacing between DO NO.
-        description3.append(sublist[3])
+        do_no.append(sublist[2])
+        description2.append(sublist[3])
         qty.append(sublist[4])
+        amount.append(sublist[6])
 
-    return for_month, do_date, do_no, description3, qty
+        # Get code contents
+        code_contents = extract_description(sublist[3])
+        code_3.append(code_contents[0])
+        code_4.append(code_contents[1])
+        
+    return for_month, do_date, do_no, description2, qty, amount, code_1, code_2, code_3, code_4
 
 
-def add_data(df_data, unique_rows, pricings, total_qty, for_month, do_date, do_no, description3, qty, ref_no, date, sub_total, location, subcon):
+def add_data(
+    df_data,
+    unique_rows,
+    pricings,
+    total_qty,
+    for_month,
+    do_date,
+    do_no,
+    description2,
+    qty,
+    amount,
+    code_1,
+    code_2,
+    code_3,
+    code_4,
+    inv_no,
+    date,
+    sub_total,
+    subcon,
+):
     """
-    Add data to dataframe
+    Add data to dataframe.
 
     Args:
-        df_data (pandas.core.frame.DataFrame): Dataframe
+        df_data (pandas.core.frame.DataFrame): Dataframe of table
         unique_rows (int): Number of unique rows
         pricings (dict): Dictionary of unique descriptions and unit prices
         total_qty (dict): Dictionary of unique descriptions and total quantities
         for_month (list): List of for_month
         do_date (list): List of do_date
         do_no (list): List of do_no
-        description3 (list): List of description3
+        description2 (list): List of description2
         qty (list): List of qty
-        ref_no (str): Reference number
-        date (str): Date
-        sub_total (float): Sub total
-        location (str): Location
-        subcon (str): Subcon
+        amount (list): List of invoice amount
+        code_1 (list): List of concrete grades
+        code_2 (list): List of slump values
+        code_3 (list): List of retardant used
+        code_4 (list): List of retardant duration
+        inv_no (str): Invoice number
+        date (str): Invoice date
+        sub_total (float): Subtotal amount
+        subcon (str): Subcon name
 
     Returns:
-        df_data (pandas.core.frame.DataFrame): Dataframe
+        df_data (pandas.core.frame.DataFrame): Dataframe of table
     """
-    df_data.loc[0, 'Ref No.'] = ref_no
-    df_data.loc[0, 'Date'] = date
-    df_data.loc[0, 'Total Amt per Inv'] = sub_total
+    df_data.loc[0, "Inv No."] = inv_no
+    df_data.loc[0, "Date"] = date
+    df_data.loc[0, "Total Amt per Inv"] = sub_total
 
-    df_data.loc[:unique_rows, 'Description'] = list(pricings.keys())
-    df_data.loc[:unique_rows, 'Total Qty'] = list(total_qty.values())
-    df_data.loc[:unique_rows, 'Unit Price'] = list(pricings.values())
+    df_data.loc[:unique_rows, "Description"] = list(pricings.keys())
+    df_data.loc[:unique_rows, "Total Qty"] = list(total_qty.values())
+    df_data.loc[:unique_rows, "Unit Rate"] = list(pricings.values())
 
-    df_data['For Month (YYYY MM)'] = for_month
-    df_data['Invoice No.'] = ref_no
-    df_data['Location'] = location
-    df_data['For TAK or Subcon? [Pintary/BBR/KKL...etc]'] = subcon
-    df_data['DO Date'] = do_date
-    df_data['DO No.'] = do_no
-    df_data['Description3'] = description3
-    df_data['Qty'] = qty
+    df_data["For Month (YYYY MM)"] = for_month
+    df_data["Ordered by TAK or Subcon? [Pintary/ BBR/ KKL..etc]"] = subcon
+    df_data["Zone"] = zone_dict[subcon.upper()]
+    df_data["DO Date"] = do_date
+    df_data["DO No."] = do_no
+    df_data["Description2"] = description2
+    df_data["Qty"] = qty
+    df_data["Vendor Invoice Amount"] = amount
+    df_data["Code1"] = None
+    df_data["Code2"] = None
+    df_data["Code3"] = code_3
+    df_data["Code4"] = code_4
 
-    df_data['Ref No.'] = pd.to_numeric(df_data['Ref No.'], errors='coerce')
-    df_data['Total Qty'] = pd.to_numeric(df_data['Total Qty'], errors='coerce')
-    df_data['Unit Price'] = pd.to_numeric(df_data['Unit Price'], errors='coerce')
-    df_data['Total Amt per Inv'] = pd.to_numeric(df_data['Total Amt per Inv'], errors='coerce')
-    df_data['Invoice No.'] = pd.to_numeric(df_data['Invoice No.'], errors='coerce')
-    df_data['Qty'] = pd.to_numeric(df_data['Qty'], errors='coerce')
+    df_data["Total Qty"] = pd.to_numeric(df_data["Total Qty"], errors="coerce")
+    df_data["Unit Rate"] = pd.to_numeric(df_data["Unit Rate"], errors="coerce")
+    df_data["Total Amt per Inv"] = pd.to_numeric(df_data["Total Amt per Inv"], errors="coerce")
+    df_data["Qty"] = pd.to_numeric(df_data["Qty"], errors="coerce")
+    df_data["Vendor Invoice Amount"] = pd.to_numeric(df_data["Vendor Invoice Amount"], errors="coerce")
 
-    df_data['Subtotal Amount'] = df_data['Total Qty'] * df_data['Unit Price']
+    df_data['Subtotal Amount'] = df_data['Total Qty'] * df_data['Unit Rate']
 
     # Add units
     for i in range(len(pricings.keys())):
         if 'UNDERLOAD CHARGES' in df_data.loc[i, 'Description']:
-            df_data.loc[i, 'Unit'] = 'TRIP'
+            df_data.loc[i, 'Unit'] = 'trip'
         else:
             df_data.loc[i, 'Unit'] = 'm3'
 
